066d3987a9f8231c5d80a397d16ab60add9ce178
CristianGarcia2021 <83602181+CristianGarcia2021@users.noreply.github.com>
2021-05-30 12:19:26 -0600

    Login

--- login.php
+++ login.php
@@ -15,23 +15,11 @@ if(isset($_SESSION['usuario']))
   <title>Bienvenido a Noti</title>
   
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
   
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   
   <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
